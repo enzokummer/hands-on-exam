e4d1045fae58c31696d10e19d941c19e642da215
Enzo Kummer <enzokummer@gmail.com>
2024-10-30 18:54:24 -0300

    question/9
    question/9

--- main.py
+++ main.py
@@ -9,4 +9,38 @@ def seven():
     new_list.sort()
     return new_list
 
+<<<<<<< Updated upstream
 #print(seven())
+=======
+def steps_to_the_right(nums, steps):
+    new_list = []
+    for i in range(steps+1):
+        new_list.append(nums[i])
+    for i in range(steps+1):
+        nums.remove(nums[0])
+
+    return nums + new_list
+
+def nine(cons):
+    output = {}
+    for consultant in cons:
+        company = consultant["company"]
+        if company not in output:
+            output[company] = []
+
+        output[company].append(consultant)
+
+#print(seven())
+
+#nums = [1,2,3,4,5,6,7]
+#print(steps_to_the_right(nums, 3))
+
+consultants = [
+{ name: 'Peyton Turner', company: 'Walker Inc' },
+{ name: 'Isaias Fritsch', company: 'Walker Inc' },
+{ name: 'Susana Wilderman', company: 'Nolan Inc' }
+]
+
+print(nine(consultants))
+
+>>>>>>> Stashed changes
